6152640d86073a19ca0bd6ddbf25573279d72d9d
DevHouda <devhouda@gmail.com>
2024-01-14 17:30:10 +0100

    15th project

--- image-resizer.py
+++ image-resizer.py
@@ -1,5 +1,18 @@
 #15
 from PIL import Image
 
-image = Image.open()
+def resize_image(dim1, dim2):
 
+    image = Image.open("image_with_face.jpg")
+
+    print(f"Current size: {image.size}")
+
+    resized_image = image.resize((dim1, dim2))
+
+    resized_image.save("image_with_face_resized.jpg")
+
+dim1 = int(input("Enter width: "))
+dim2 = int(input("Enter height: "))
+
+resize_image(dim1, dim2)
+print("Image Resized.")
