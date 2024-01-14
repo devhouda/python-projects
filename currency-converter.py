ece021a611350a2d74a492c160b55fda6d3a3873
DevHouda <devhouda@gmail.com>
2024-01-14 13:34:55 +0100

    10th project

---
+++ currency-converter.py
@@ -0,0 +1,14 @@
+
+convert_to_pounds = lambda dollars : dollars * 0.82
+
+def main():
+    print("This program converts US dollars to Pounds Sterling")
+    print()
+
+    dollars = eval(input("Enter amount in dollars: "))
+
+    pounds = convert_to_pounds(dollars)
+
+    print(f"That is {pounds} pounds.")
+
+main()
